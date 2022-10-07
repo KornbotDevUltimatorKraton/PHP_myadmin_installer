3e72cd498657065a6433be6997159f819f97fc3c
Chanapai Chuadchum <kornbot359@hotmail.com>
2022-10-07 20:43:39 +0700

    Update phpmyadmin_installer.sh

--- phpmyadmin_installer.sh
+++ phpmyadmin_installer.sh
@@ -1,4 +1,5 @@
 sudo apt-get update && sudo apt-get upgrade -y 
+sudo apt-get install apache2 -y 
 sudo apt-get install lamp-server^ -y 
 sudo apt-get install phpmyadmin -y 
 sudo ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf-available/phpmyadmin.conf
